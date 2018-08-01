fba3f0eb740be94f49fd08a869b82f040607e86c
willy <wilg1569@gmail.com>
2018-08-01 17:50:17 +0200

    movie db

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Movie.destroy_all
+Director.destroy_all
 
-movies = Movie.create([{title: "The Matrix", release_year: 1999, director: "Wachowski sisters"}, {title: "Watchmen", release_year: 2009, director: "Zack Snyder"}, {title: "Django Unchained", release_year: 2012, director: "Quentin Tarantino"}, {title: "Brimstone", release_year: 2016, director: "Martin Koolhoven"}, {title: "Old Boy", release_year: 2003, director: "Park Chan-wook"}])
+#director = Director.create([{first_name: "Lana / Lilly", last_name: "Wachowski"}, {first_name: "Zack", last_name: "Snyder"}, {first_name: "Quentin", last_name: "Tarantino"}, {first_name: "Martin", last_name: "Koolhoven"}, {first_name: "Park", last_name: "Chan-wook"}])
+#movie = Movie.create([{title: "The Matrix", release_year: 1999}, {title: "Watchmen", release_year: 2009}, {title: "Django Unchained", release_year: 2012}, {title: "Brimstone", release_year: 2016}, {title: "Old Boy", release_year: 2003}])
+
+director = Director.create(first_name: "Lana / Lilly", last_name: "Wachowski")
+director.movies.create(title: "The Matrix", release_year: 1999)
+
+director = Director.create(first_name: "Zack", last_name: "Snyder")
+director.movies.create(title: "Watchmen", release_year: 2009)
+
+director = Director.create(first_name: "Quentin", last_name: "Tarantino")
+director.movies.create(title: "Django Unchained", release_year: 2012)
+
+director = Director.create(first_name: "Martin", last_name: "Koolhoven")
+director.movies.create(title: "Brimstone", release_year: 2016)
+
+director = Director.create(first_name: "Park", last_name: "Chan-wook")
+director.movies.create(title: "Old Boy", release_year: 2003)
