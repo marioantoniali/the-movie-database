321a68d42f4b591367717ef5a8b8b11e2d8e9479
willy <wilg1569@gmail.com>
2018-08-01 14:55:35 +0200

    Creation index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+movies = Movie.create([{title: "The Matrix", release_year: 1999, director: "Wachowski sisters"}, {title: "Watchmen", release_year: 2009, director: "Zack Snyder"}, {title: "Django Unchained", release_year: 2012, director: "Quentin Tarantino"}, {title: "Brimstone", release_year: 2016, director: "Martin Koolhoven"}, {title: "Old Boy", release_year: 2003, director: "Park Chan-wook"}])
